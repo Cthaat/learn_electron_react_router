4be41a6bebd829666f32d967d2f4767fb9fe4f7d
Edge <edge27452696803@gmail.com>
2025-04-27 13:50:39 +0800

    Refactor code structure for improved readability and maintainability

---
+++ src/electron/resourceManager.ts
@@ -0,0 +1,11 @@
+import osUtil from "os-utils";
+
+const POLLING_INTERVAL = 1000; // 1 second
+
+export function pollResource() {
+  setInterval(() => {}, POLLING_INTERVAL);
+}
+
+function getCpuUsage() {
+  osUtil;
+}
