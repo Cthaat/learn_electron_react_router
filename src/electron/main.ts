480d818f27d1ba70187ed0db2859bd24add4c721
Edge <edge27452696803@gmail.com>
2025-04-27 22:07:20 +0800

    添加预加载脚本支持，优化主进程与渲染进程的数据交互

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -2,9 +2,15 @@ import { app, BrowserWindow } from "electron";
 import path from "path";
 import { isDev } from "./util.js";
 import { pollResource } from "./resourceManager.js";
+import { get } from "http";
+import { getPreloadPath } from "./pathResolver.js";
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+  });
   if (isDev()) {
     mainWindow.loadURL("http://localhost:5123");
   } else {
